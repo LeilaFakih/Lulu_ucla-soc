e19193d160a8115a6908f288188e3af07e679a9a
lulu <lulu@Mayas-MacBook-Air-2.local>
2018-07-12 12:08:42 -0700

    Update resources

--- students.js
+++ students.js
@@ -1,2 +1,3 @@
 var students = ['Lee', 'William', 'Leila', 'Brandon', 'Jordan', 'Josh', 'Billy', 'Jimmy', 'Jeremy', 'Tom']
 var awesome= students.map(student => student + " is awesome");
+students.length 
